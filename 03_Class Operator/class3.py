0eb070d53dea134e2645fa12542d8f2a43959c29
Ahtasham Shafique <139730549+ahtashams1212@users.noreply.github.com>
2023-10-22 15:34:13 +0500

    Add files via upload

---
+++ 03_Class Operator/class3.py
@@ -0,0 +1,14 @@
+# Arithmetic operator
+a : int = 56
+b : int = 8
+
+print(a + b) # addtition
+print(a - b) # subtraction
+print(a * b) # multiplication
+print(a / b) # division
+
+    # modulus operator
+a : int = 16
+b : int = 3
+
+print(a % 3) # remaining value or modulus
